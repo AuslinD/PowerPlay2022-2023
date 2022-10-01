52ba40586190471bb4465d6046e5d8ee0c80737f
BlueDeathMage <ryan111chui@gmail.com>
2022-10-01 15:27:50 -0500

    create megaman
    megaman is cool

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/megaman.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+@TeleOp(name = "test teleop2",group = "test teleop2")
+public class megaman extends OpMode {
+    public DcMotor fl;
+    public DcMotor bl;
+    public DcMotor fr;
+    public DcMotor br;
+
+    BNO055IMU imu;
+
+    Orientation angles;
+    Acceleration gravity;
+
+
+
+
+    @Override
+    public void init() {
+        fl = hardwareMap.dcMotor.get("fl");
+        bl = hardwareMap.dcMotor.get("bl");
+        fr = hardwareMap.dcMotor.get("fr");
+        br = hardwareMap.dcMotor.get("br");
+
+    }
+
+    @Override
+    public void loop() {
+        
+    }
+}
