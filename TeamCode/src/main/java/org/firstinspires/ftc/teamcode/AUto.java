c045525296abf2d676f69b0ec55e54a83d344559
BlueDeathMage <ryan111chui@gmail.com>
2022-10-19 18:07:57 -0500

    Update AUto.java
    idk what im doing :moyai:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUto.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 public class AUto {
+    Robot robot;
+    private PID pid = new PID();
+
+
+    public void drive(double distance, double speed, boolean isForward, int timeout){
+        fl.setPower(pid.loop(distance, timeout));
+        bl.setPower(pid.loop(distance, timeout));;
+        br.setPower(-pid.loop(distance, timeout));;
+        fr.setPower(-pid.loop(distance, timeout));;
+    }
+
+    public void turn(Angle angle, LinearOpMode LinearOpMode){
+
+    }
 }
