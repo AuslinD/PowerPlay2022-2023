c9418a9046a974bf720910844f3029716eef9712
BlueDeathMage <ryan111chui@gmail.com>
2022-10-24 18:06:12 -0500

    working on AUto
    :beginner:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
@@ -9,6 +11,8 @@ public class Robot {
 
     private OpMode teleOp;
     private LinearOpMode auto;
+    BNO055IMU imu;
+
 
     public Robot(LinearOpMode opMode){
         auto = opMode;
@@ -16,6 +20,19 @@ public class Robot {
         drivetrain = new Drivetrain(opMode);
         manip = new Manipulator(opMode);
 
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = opMode.hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
 
         opMode.telemetry.addLine("Crabtrain Init Completed - Linear");
         opMode.telemetry.update();
@@ -31,5 +48,7 @@ public class Robot {
         opMode.telemetry.update();
     }
 
-
+    public Drivetrain getDrivetrain() {
+        return drivetrain;
+    }
 }
