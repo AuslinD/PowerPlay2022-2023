067bd2aa515dfa8f00f217b126a7829f7ef2de99
austindeng <austindeng3@gmail.com>
2022-12-14 16:51:10 -0600

    RR setup kind of

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MotorDirectionDebugger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MotorDirectionDebugger.java
@@ -40,7 +40,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  */
 
 @Config
-@TeleOp(group = "drive")
+@TeleOp(group = "drive", name = "motorTest")
 public class MotorDirectionDebugger extends LinearOpMode {
     public static double MOTOR_POWER = 0.7;
 
